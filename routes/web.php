227ffc0ec2ed61fe18f49a40c00d47c7db7eaa0f
EC2 Default User <nta376612@gmail.com>
2023-09-06 15:07:33 +0900

    delete add

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::put("/post/{post}",[PostController::class,"update"]);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
+

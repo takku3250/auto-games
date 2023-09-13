0f8b15ee50ca3aa5997b00fd07a147742aa190a8
EC2 Default User <nta376612@gmail.com>
2023-09-13 17:25:39 +0900

    mis

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -16,6 +16,7 @@
     <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('patch')
+      
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
